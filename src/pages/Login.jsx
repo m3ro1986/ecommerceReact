2f3253f20f597aae645d5a6d29437896c04a6fe6
m3ro1986 <m3ro_1986@outlook.com>
2023-02-02 22:43:34 -0600

    ah¿juste

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -49,9 +49,6 @@ const Login = () => {
                         { ...register( 'password')}
                     />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
                 <Button variant="primary" type="submit">
                     Submit
                 </Button>
